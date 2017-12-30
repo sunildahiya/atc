a216200c9270e702678708083729e03834465ad9
sunildahiya <sainisunil302@gmail.com>
2017-12-31 01:03:09 +0530

    Serial print util functions are added

--- Navigation/Serial_Print.ino
+++ Navigation/Serial_Print.ino
@@ -1,35 +1,82 @@
+void serial_two(String str, double value){
+  Serial.print(str);
+  Serial.print("\t");
+  Serial.print(value);
+}
+
+void serial_twoln(String str, double value){
+  Serial.print(str);
+  Serial.print("\t");
+  Serial.println(value);
+}
+
+void serial_two(String str, int value){
+  Serial.print(str);
+  Serial.print("\t");
+  Serial.print(value);
+}
+
+void serial_twoln(String str, int value){
+  Serial.print(str);
+  Serial.print("\t");
+  Serial.println(value);
+}
+
+void serial_tab(double value){
+  Serial.print(value);
+  Serial.print("\t");
+}
+
+
 void serial_print(){
 //  read_ultra();
 //  find_moving_avg();
-  Serial.print(stage);
-  Serial.print("\t");
-  Serial.print(leftFrontDist);
-  Serial.print("\t"); 
-  Serial.print(leftBackDist);
-  Serial.print("\t");
-  Serial.print(movingAvgLeftFrontV);
-  Serial.print("\t"); 
-  Serial.print(movingAvgLeftBackV);
-  Serial.print("\t");
-  Serial.print(frontLeftDist);
-  Serial.print("\t"); 
-  Serial.print(frontRightDist);
-  Serial.print("\t");
-  Serial.print(movingAvgFrontLeftV);
-  Serial.print("\t"); 
-  Serial.print(movingAvgFrontRightV);
+  serial_tab(stage);
+  serial_tab(leftFrontDist);
+  serial_tab(leftBackDist);
+  serial_tab(movingAvgLeftFrontV);
+  serial_tab(movingAvgLeftBackV);
+  serial_tab(frontLeftDist);
+  serial_tab(frontRightDist);
+  serial_tab(movingAvgFrontLeftV);
+  serial_tab(movingAvgFrontRightV);
+  serial_tab(gyroZangle);
+  serial_tab(angleError);
+  serial_tab(xError);
+  serial_tab(error);
+  serial_tab(derror);
+  Serial.println(correction);
+  
+//  Serial.print(stage);
+//  Serial.print("\t");
+//  Serial.print(leftFrontDist);
+//  Serial.print("\t"); 
+//  Serial.print(leftBackDist);
+//  Serial.print("\t");
+//  Serial.print(movingAvgLeftFrontV);
+//  Serial.print("\t"); 
+//  Serial.print(movingAvgLeftBackV);
+//  Serial.print("\t");
+//  Serial.print(frontLeftDist);
+//  Serial.print("\t"); 
+//  Serial.print(frontRightDist);
+//  Serial.print("\t");
+//  Serial.print(movingAvgFrontLeftV);
+//  Serial.print("\t"); 
+//  Serial.print(movingAvgFrontRightV);
+//  Serial.print("t");
 //  Serial.print("\t");
 //  Serial.print(leftRefDist);
-  Serial.print("\t");
-  Serial.print(angleError);
-  Serial.print("\t");
-  Serial.print(xError);
-  Serial.print("\t");  
-  Serial.print(error);
-  Serial.print("\t");
-  Serial.print(derror);
-  Serial.print("\t");
-  Serial.println(correction);
+//  Serial.print("\t");
+//  Serial.print(angleError);
+//  Serial.print("\t");
+//  Serial.print(xError);
+//  Serial.print("\t");  
+//  Serial.print(error);
+//  Serial.print("\t");
+//  Serial.print(derror);
+//  Serial.print("\t");
+//  Serial.println(correction);
 
 //  Serial.print(frontLeftDist);
 //  Serial.print("\t"); 
@@ -55,5 +102,34 @@ void serial_print(){
 //  Serial.print(derror);
 //  Serial.print("\t");
 //  Serial.println(correction);
+
+
+
+//    Serial.print("sv_clean1FrontStop");
+//    Serial.print("\t"); 
+//    Serial.print("angleError");
+//    Serial.print("\t"); 
+//    Serial.print("leftBackDist");
+//    Serial.print("\t");
+//    Serial.println("sv_clean1LeftStopF");
+//
+//    
+//    Serial.print(sv_clean1FrontStop);
+//    Serial.print("\t"); 
+//    Serial.print(angleError);
+//    Serial.print("\t");
+//    Serial.print(leftBackDist);
+//    Serial.print("\t");
+//    Serial.println(sv_clean1LeftStopF);
+//    delay(60000);
+//    stage = s_stop;
+}
+
+void serial_input_ctrl(){
+  if (Serial.available()){
+    int v = Serial.parseInt();
+    if (v == 1)
+      stage = s_stop;
+  } 
 }
 
